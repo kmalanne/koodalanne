ceb7f76b25c029243dac58a7b52071ea8e584a9e
Kai-Mikael Alanne <kaimikael.alanne@icloud.com>
2023-05-22 18:22:31 +0300

    Setup

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,10 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {},
+  "app": {
+    "/Users/Kaitsu/Documents/Code/workingtitle/node_modules/next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Press_Start_2P\",\"arguments\":[{\"weight\":\"400\",\"subsets\":[\"latin\"]}],\"variableName\":\"pressStart2P\"}": [
+      "static/media/9b8c15de1de72117-s.p.woff2"
+    ]
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": false
+}
