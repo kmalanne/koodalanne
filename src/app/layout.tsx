cc3b4b3ecb02c1afb3fed439313b3b78f21de105
Kai-Mikael Alanne <kaimikael.alanne@icloud.com>
2023-10-18 16:46:28 +0300

    Content and UI changes + pnpm.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,37 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Workingtitle',
-  description: 'Workingtitle website',
+  title: 'koodalanne',
+  description: 'koodalanne freelance developer website',
+  applicationName: 'koodalanne',
+  referrer: 'origin-when-cross-origin',
+  keywords: [
+    [
+      'Freelance software developer',
+      'Web development',
+      'Frontend development',
+      'Backend development',
+      'Fullstack development',
+      'JavaScript',
+      'TypeScript',
+      'NextJS',
+      'ReactJS',
+      'HTML/CSS',
+      'SQL',
+      'Azure',
+      'AWS',
+      'Website maintenance',
+      'Website optimization',
+      'Cloud engineering',
+      'DevOps',
+      'Tech lead',
+      'Web-kehittäjä',
+      'Ohjelmistokehittäjä',
+      'Kehittäjä',
+      'Koodari',
+    ],
+  ],
+  authors: [{ name: 'Kai-Mikael Alanne' }],
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
